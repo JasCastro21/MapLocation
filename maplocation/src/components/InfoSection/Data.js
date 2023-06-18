b5f3852b217349be3a2a33452b2997e10f25c440
JasCastro21 <jasminecastr0@edu.unifor.br>
2023-06-18 19:23:07 -0300

    Pagina Sobre

---
+++ maplocation/src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'MapLocation Pramium',
+    headline: 'Pesquisas ilimitadas sem custos adicionais',
+    description: "Com o Pramium, nosso site oferece acesso gratuito e ilimitado a informações de localização. Encontre endereços, pontos de interesse e empresas sem custos adicionais. Navegue no mapa, pesquise por localização ou explore categorias específicas. Descubra tudo o que precisa, sem limites ou taxas extras. Comece a explorar agora mesmo!",
+    buttonLabel: 'Começar!',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
